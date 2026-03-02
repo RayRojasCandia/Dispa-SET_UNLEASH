863b3adce9a447e47ec049e96fa7cd794f67c28b
Ray <40904107+RayRojasCandia@users.noreply.github.com>
2026-03-02 20:44:15 +0100

    Updating little changes into the scripts

--- scripts/read_results_Boundary_Sector_UNLEASH.py
+++ scripts/read_results_Boundary_Sector_UNLEASH.py
@@ -13,14 +13,14 @@ sys.path.append(os.path.abspath('..'))
 import dispaset as ds
 
 # Load the inputs and the results of the simulation
-inputs,results = ds.get_sim_results(path='../Simulations/PyPSA_ DispaSET_Unleash/Reference/Sect_Power_2030',cache=False)
+inputs,results = ds.get_sim_results(path='../Simulations/PyPSA_ DispaSET_Unleash/Sufficiency/Sect_Power_VPP_2030_0.1',cache=False)
 
 # if needed, define the plotting range for the dispatch plot:
 import pandas as pd
-rng = pd.date_range(start='2030-01-01',end='2030-01-07',freq='h')
+rng = pd.date_range(start='2030-06-01',end='2030-06-07',freq='h')
 
 # Generate country-specific plots
-ds.plot_zone(inputs,results,z='DE',rng=rng)
+ds.plot_zone(inputs,results,z='BE',rng=rng)
 
 # Bar plot with the installed capacities in all countries:
 cap = ds.plot_zone_capacities(inputs,results)
